99191c0d9b2a3e17f8cf0e0f3c23a43c5fa29495
cenitelas <sanyaug777888>
2020-03-29 11:55:04 +0600

    +

--- social/settings.py
+++ social/settings.py
@@ -68,7 +68,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'cenitelas1@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 's'
 EMAIL_PORT = 587
 EMAIL_USE_SSL = False
 # Application definition
